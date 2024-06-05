5ae5b8f51a0fe82536cc8516de82446818c8d3ce
povpich12030303 <159798821+povpich12030303@users.noreply.github.com>
2024-06-05 14:58:55 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,65 @@
+// function text() {
+//   document.getElementById("Pich").style.backgroundColor = "pink";
+// }
+// let book = 20;
+// let vat = 15;
+// function total() {
+//   document.write(1 + 2 * 3);
+// }
+// total();
+// const pich = "Hello";
+// console.log(pich);
+
+//const ID_Student = ["Pich", "Bormey", "Kenzy", "Tyy"];
+// function Hello() {
+//   console.log("Hello World!");
+// }
+// Hello();
+// let Pich = function () {
+//   console.log(4 + 5);
+// };
+// Pich();
+// const Pich = (time) => {
+//   console.log("Hello wordl" + time);
+// };
+// Pich();
+// const i = 1;
+// while (i <= 10) {
+//   console.log("Hello wordl");
+//   i++;
+// }
+// const login = document.querySelector("#form");
+
+// login.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const Username = document.querySelector(".username").value;
+//   const Password = document.querySelector(".password").value;
+
+//   // Simulate login validation (replace with your backend interaction)
+//   if (Username === "Pich" && Password === "120303") {
+//     // Login successful
+//     alert("Login successful!");
+//   } else {
+//     alert("Invalid username or password.");
+//   }
+// });
+// const Button = document.querySelector(".btn");
+// const Button2 = document.querySelector(".btn2");
+// const text = document.querySelector("h2");
+
+// Button.onmouseover = () => (text.style.color = "Red");
+// Button.onmouseout = () => (text.style.color = "green");
+
+// const url = "./project1.html";
+// Button.addEventListener("click", function () {
+//   url.open = url;
+// });
+// const Btn = document.querySelector(".btnn");
+// const BTN = document.querySelector(".Bot");
+// Btn.addEventListener("click", () => {
+//   window.open("https://facebook.com");
+// });
+// BTN.addEventListener("click", () => {
+//   window.open("https:// ");
+// });
